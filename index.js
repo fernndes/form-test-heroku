7c51761bd35046b59da78848f7f0f2401d8c69c9
fernndes <you@example.com>
2021-01-02 20:11:22 -0300

    test

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.post('/insert', async function(req, res) {
 
 	const {name, email, age, contact} = req.body;
 
-	const result = await insert(name, email, age, contact);
+	//const result = await insert(name, email, age, contact);
 
-	res.send(result)
+	res.send(name, email, age, contact)
 })
 
 app.listen(process.env.PORT || port)
