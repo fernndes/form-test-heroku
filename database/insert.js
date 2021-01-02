b08b9ce78ffedb2fac2c225257c714b9a3128f9f
fernndes <you@example.com>
2021-01-02 20:17:11 -0300

    test

--- database/insert.js
+++ database/insert.js
@@ -5,12 +5,7 @@ const insert = async function insertData(name, email, age, phone){
 
 	await db.query('INSERT INTO usuarios(nome, email, idade, telefone) VALUES ($1, $2, $3, $4)', 
 		[name, email, age, phone]
-		).then(() => {
-
-		})
-		.catch(err => {
-			console.log(err);
-		});
+		)
 
 	await db.end();
 }
