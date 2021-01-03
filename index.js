ab3a658800a232a77dae3250f3f0c70fd1ef0cee
fernndes <you@example.com>
2021-01-02 21:28:37 -0300

    fixing some errors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const insert = require('./database/insert')
 
 app.use(express.urlencoded())
 
-app.post('/insert', async function(req, res) {
+app.post('/insert', function(req, res) {
 
 	const [name, email, age, contact] = req.body;
 
