732c28c8114c8b8742d8cd499646d152192d3b98
fernndes <you@example.com>
2021-01-02 23:42:01 -0300

    fixing some bugs

--- database/insert.js
+++ database/insert.js
@@ -1,9 +1,9 @@
 const db = require("./config");
 
-const insert = async function insertData(name, email, age, phone){
-	await db.connect();
+var status;
 
-	var status
+const insert = async function insertData(name, email, age, phone){
+	await db.connect();	
 
 	await db.query('INSERT INTO usuarios3(nome, email, idade, telefone) VALUES ($1, $2, $3, $4)', 
 		[name, email, age, phone], (err, res) => {
