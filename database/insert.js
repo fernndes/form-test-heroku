1e394f078c83484de87e014132b289d34c813f4e
fernndes <you@example.com>
2021-01-02 23:33:34 -0300

    fixing some bugs

--- database/insert.js
+++ database/insert.js
@@ -3,18 +3,22 @@ const db = require("./config");
 const insert = async function insertData(name, email, age, phone){
 	await db.connect();
 
+	var status
+
 	await db.query('INSERT INTO usuarios3(nome, email, idade, telefone) VALUES ($1, $2, $3, $4)', 
 		[name, email, age, phone], (err, res) => {
 			if (err) {
-				await db.end();
 	    		console.log(err);
-	    		return err
+	    		status = err
 			 } else {
-			 	await db.end();
 			 	console.log("Sucesso");
-			 	return "<h1>Cadastro efetuado com sucesso</h1>"
+			 	status = "Cadastro efetuado com sucesso"
 			  }
 		})
+
+	await db.end();
+
+	return status
 }
 
 module.exports = insert;
