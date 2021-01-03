2baab70bd1e6c9d6b00e249a13e612dac38258ba
fernndes <you@example.com>
2021-01-02 21:18:39 -0300

    fixing some errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-var insert = require('./database/insert')
+const insert = require('./database/insert')
 
 app.use(express.urlencoded())
 
